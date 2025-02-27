9676e15b166a46c5b833cb6c07361723afe903b8
Carla Lopez <calopez@student.ethz.ch>
2025-02-27 13:34:43 +0100

    add counts to scpobaf

--- scaling/flops/scpobaf_flops.py
+++ scaling/flops/scpobaf_flops.py
@@ -2,20 +2,16 @@
 def scpobaf_flops(n_diagonals, n_offdiags, arrowhead_blocksize):
 
     # Process banded part of the matrix
-    FLOPS:int = 0
+    FLOPS: int = 0
 
     counts = {
-        'triangular_solve_nb3': 0,
         'vector_scaling_ns': 0,
         'vector_scaling_nb': 0,
-        'element_scaling': 0,
         'div': 0,
         'sqrt': 0,
         'dot_product_ns': 0,
-        'dot_product_nb': 0,
         'matrix_vector_nsns': 0,
         'matrix_vector_nsnb': 0,
-        'matrix_vector_nbnb': 0,
         'DGEMM_nb3': 0,
         'GER_nb2': 0
     }
@@ -27,39 +23,53 @@ def scpobaf_flops(n_diagonals, n_offdiags, arrowhead_blocksize):
         FLOPS += 1  # 1 sqrt
 
         counts['div'] += 1
-        FLOPS += 1 # 1 div
+        FLOPS += 1  # 1 div
 
         # Update column i of the lower diagonals
-        FLOPS += 2 * n_offdiags*(n_offdiags-1)  # matrix product, 1 mult, 1 add
+        # matrix times vector (ns-1)*ns
+        counts['matrix_vector_nsns'] += 1
+        FLOPS += 2 * n_offdiags*(n_offdiags-1)
 
         # L_{i+1, i} = A_{i+1, i} @ L_{i, i}^{-T}
+        counts['vector_scaling_ns'] += 1
         FLOPS += n_offdiags  # vector scaling ns
 
         # L_{ndb+1, i} = A_{ndb+1, i} @ L_{i, i}^{-T}
+        counts['vector_scaling_nb'] += 1
         FLOPS += arrowhead_blocksize  # vector scaling nb
 
-        b =  i+1-max(0, i-n_offdiags+1)
-        # FLOPS += 2 * arrowhead_blocksize * n_offdiags  # matrix times vector (ns-1)*ns
-        FLOPS += 2 * arrowhead_blocksize * b  # matrix times vector (ns-1)*ns
+        # A_{ndb+1, i+1} = A_{ndb+1, i+1} - L_{ndb+1, i} @ L_{i+1, i}.conj().T
+        b = i+1-max(0, i-n_offdiags+1)
+        counts['matrix_vector_nsnb'] += 1
+        FLOPS += 2 * arrowhead_blocksize * b
 
-        # A_{ndb+1, ndb+1} = A_{ndb+1, ndb+1} - L_{ndb+1, i} @ L_{ndb+1, i}.conj().T
-        FLOPS += 2 * arrowhead_blocksize**2  # outer product, one mult one add
+        # A_{ndb+1, ndb+1} = A_{ndb+1, ndb+1} - L_{ndb+1, i} @ L_{ndb+1,
+        # i}.conj().T
+        counts['GER_nb2'] += 1
+        FLOPS += 2 * arrowhead_blocksize**2  # outer product
 
         # Update next diagonal
         # A_{i+1, i+1} = A_{i+1, i+1} - L_{i+1, i} @ L_{i+1, i}.conj().T
+        counts['dot_product_ns'] += 1
         FLOPS += 2 * n_offdiags  # dot product
 
     # L_{ndb, ndb} = chol(A_{ndb, ndb})
+    counts['sqrt'] += 1
     FLOPS += 1  # 1 sqrt
 
     # L_{ndb+1, ndb} = A_{ndb+1, ndb} @ L_{ndb, ndb}^{-T}
-    FLOPS += 1 # div
+    counts['div'] += 1
+    FLOPS += 1  # div
+    counts['vector_scaling_nb'] += 1
     FLOPS += arrowhead_blocksize  # vector scaling nb
 
     # A_{ndb+1, ndb+1} = A_{ndb+1, ndb+1} - L_{ndb+1, ndb} @ L_{ndb+1, ndb}^{T}
+    counts['GER_nb2'] += 1
     FLOPS += 2 * arrowhead_blocksize**2  # outer product, one mult one add
 
     # L_{ndb+1, ndb+1} = chol(A_{ndb+1, ndb+1})
-    FLOPS += 1/3*arrowhead_blocksize**3 + 1/2*arrowhead_blocksize**2 + 1/6*arrowhead_blocksize  # cholesky
+    counts['cholesky_nb'] += 1
+    FLOPS += 1/3*arrowhead_blocksize**3 + 1/2 * \
+        arrowhead_blocksize**2 + 1/6*arrowhead_blocksize  # cholesky
 
     return int(FLOPS), counts
