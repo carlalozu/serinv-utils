8657617a843694221514a065192c18b870c5ce02
Carla Lopez <calopez@student.ethz.ch>
2025-02-27 14:46:38 +0100

    ALG_OPERATION_COUNT

---
+++ scaling/flops/const.py
@@ -0,0 +1,152 @@
+
+OPERATIONS_FLOPS = {
+    'cholesky_ns3': {
+        'name': 'potrf $n_s^3$',
+        'flops': lambda ns, nb: 1/3*ns**3+1/2*ns**2+1/6*ns,
+    },
+    'cholesky_nb3': {
+        'name': 'potrf $n_b^3$',
+        'flops': lambda ns, nb: 1/3*nb**3+1/2*nb**2+1/6*nb,
+    },
+    'triang_solve_ns3': {
+        'name': 'trsm $n_s^3$',
+        'flops': lambda ns, nb: ns**3,
+    },
+    'triang_solve_nb3': {
+        'name': 'trsm $n_b^3$',
+        'flops': lambda ns, nb: nb**3,
+    },
+    'triang_solve_ns2nb': {
+        'name': 'trsm $n_s^2n_b$',
+        'flops': lambda ns, nb: ns**2*nb,
+    },
+    'dgemm_ns3': {
+        'name': 'gemm $n_s^3$',
+        'flops': lambda ns, nb: 2*ns**3,
+    },
+    'dgemm_nb3': {
+        'name': 'gemm $n_b^3$',
+        'flops': lambda ns, nb: 2*nb**3,
+    },
+    'dgemm_ns2nb': {
+        'name': 'gemm $n_s^2n_b$',
+        'flops': lambda ns, nb: 2*ns**2*nb,
+    },
+    'dgemm_nsnb2': {
+        'name': 'gemm $n_sn_b^2$',
+        'flops': lambda ns, nb: 2*ns*nb**2,
+    },
+    'dgemm_nb2ns': {
+        'name': 'gemm $n_b^2n_s$',
+        'flops': lambda ns, nb: 2*ns*nb**2,
+    },
+    'matrix_vector_nsns': {
+        'name': 'gemv $n_s^2$',
+        'flops': lambda ns, nb: 2*ns**2,
+    },
+    'matrix_vector_nsns_1': {
+        'name': 'gemv $n_sn_{s-1}$',
+        'flops': lambda ns, nb: 2*ns*(ns-1),
+    },
+    'matrix_vector_nsnb': {
+        'name': 'gemv $n_sn_b$',
+        'flops': lambda ns, nb: 2*ns*nb,
+    },
+    'matrix_vector_nbnb': {
+        'name': 'gemv $n_b^2$',
+        'flops': lambda ns, nb: 2*nb**2,
+    },
+    'dot_prod_ns': {
+        'name': 'dot $n_s$',
+        'flops': lambda ns, nb: 2*ns,
+    },
+    'dot_prod_nb': {
+        'name': 'dot $n_b$',
+        'flops': lambda ns, nb: 2*nb,
+    },
+    'scale_ns': {
+        'name': 'scal $n_s$',
+        'flops': lambda ns, nb: ns,
+    },
+    'scale_nb': {
+        'name': 'scal $n_b$',
+        'flops': lambda ns, nb: nb,
+    },
+    'div': {
+        'name': 'div',
+        'flops': lambda ns, nb: 1,
+    },
+    'sqrt': {
+        'name': 'sqrt',
+        'flops': lambda ns, nb: 1,
+    },
+    'mult': {
+        'name': 'mult',
+        'flops': lambda ns, nb: 1,
+    },
+    'GER_nb2': {
+        'name': 'ger $n_b^2$',
+        'flops': lambda ns, nb: 2*nb**2,
+    },
+}
+
+ALG_OPERATION_COUNT = {
+    'POBAF': {
+        'scale_ns': lambda nt, n: nt-1,
+        'scale_nb': lambda nt, n: nt,
+        'div': lambda nt, n: nt,
+        'sqrt': lambda nt, n: nt,
+        'dot_prod_ns': lambda nt, n: nt-1,
+        'matrix_vector_nsns_1': lambda nt, n: nt-1,
+        'matrix_vector_nsnb': lambda nt, n: nt-1,
+        'GER_nb2': lambda nt, n: nt,
+        'cholesky_nb3': lambda nt, n: 1,
+    },
+    'POBASI': {
+        'triang_solve_nb3': lambda nt, n: 1,
+        'scale_ns': lambda nt, n: nt-1,
+        'scale_nb': lambda nt, n: nt,
+        'mult': lambda nt, n: nt,
+        'div': lambda nt, n: nt,
+        'dot_prod_ns': lambda nt, n: nt-1,
+        'dot_prod_nb': lambda nt, n: nt,
+        'matrix_vector_nsns': lambda nt, n: nt-1,
+        'matrix_vector_nsnb': lambda nt, n: nt-1,
+        'matrix_vector_nbnb': lambda nt, n: nt,
+        'dgemm_nb3': lambda nt, n: 1,
+    },
+    'POBTAF': {
+        'dgemm_ns3': lambda nt, n: nt-1,
+        'dgemm_ns2nb': lambda nt, n: nt-1,
+        'dgemm_nb2ns': lambda nt, n: nt,
+        'triang_solve_ns3': lambda nt, n: nt-1,
+        'triang_solve_ns2nb': lambda nt, n: nt,
+        'cholesky_ns3': lambda nt, n: nt,
+        'cholesky_nb3': lambda nt, n: 1,
+    },
+    'POBTASI': {
+        'triang_solve_ns3': lambda nt, n: nt,
+        'triang_solve_nb3': lambda nt, n: 1,
+        'dgemm_ns3': lambda nt, n: 4*(nt-1)+1,
+        'dgemm_ns2nb': lambda nt, n: 4*(nt-1)+2,
+        'dgemm_nsnb2': lambda nt, n: nt,
+        'dgemm_nb3': lambda nt, n: 1,
+    },
+    'POBBAF': {
+        'dgemm_ns3': lambda nt, n: (nt-1)*n**2,
+        'dgemm_ns2nb': lambda nt, n: (nt-1)*n,
+        'dgemm_nb2ns': lambda nt, n: nt,
+        'triang_solve_ns3': lambda nt, n: (nt-1)*n,
+        'triang_solve_ns2nb': lambda nt, n: nt,
+        'cholesky_ns3': lambda nt, n: nt,
+        'cholesky_nb3': lambda nt, n: 1,
+    },
+    'POBBASI': {
+        'triang_solve_ns3': lambda nt, n: nt,
+        'triang_solve_nb3': lambda nt, n: 1,
+        'dgemm_ns3': lambda nt, n: (nt-1)*(n+1)**2+1,
+        'dgemm_ns2nb': lambda nt, n: 2*(nt-1)*(n+1)+2,
+        'dgemm_nsnb2': lambda nt, n: nt,
+        'dgemm_nb3': lambda nt, n: 1,
+    },
+}
--- scaling/flops/flops.py
+++ scaling/flops/flops.py
@@ -1,3 +1,6 @@
+from serinv_utils.scaling.flops.const import OPERATIONS_FLOPS, ALG_OPERATION_COUNT
+
+
 def T_flops_POBAF(nt: int, ns: int, nb: int):
     return int(0
                + (1/3) * nb**3 + (1/2) * nb**2 + (1/6) * nb  # cholesky nb
@@ -6,10 +9,11 @@ def T_flops_POBAF(nt: int, ns: int, nb: int):
                + (ns)*(nt-1)        # vector scaling ns
                + (nb)*(nt)          # vector scaling nb
                + (2*ns)*(nt-1)      # dot product
-               + (2*(ns-1)*ns)*(nt-1) # matrix product, ns-1*ns
+               + (2*(ns-1)*ns)*(nt-1)  # matrix product, ns-1*ns
                + (2*nb*ns)*(nt-1)   # matrix product, nb*ns
                + (2*nb**2)*(nt)     # outer product
                )
+
 
 def T_flops_POBASI(nt: int, ns: int, nb: int):
     return int(0
@@ -21,7 +25,7 @@ def T_flops_POBASI(nt: int, ns: int, nb: int):
                + (2*ns)*(nt-1)      # dot product ns
                + (2*nb)*(nt)        # dot product nb
                + (2*ns**2)*(nt-1)   # matrix_vector_nsns
-               + (2*nb*ns)*2*(nt-1) # matrix_vector_nsnb
+               + (2*nb*ns)*2*(nt-1)  # matrix_vector_nsnb
                + (2*nb**2)*(nt)     # matrix_vector_nbnb
                + (2*nb**3)*(1)      # DGEMM nb3
                )
@@ -35,7 +39,7 @@ def T_flops_theory(nt: int, ns: int, nb: int):
                + (ns)*(nt-1)        # vector scaling ns
                + (nb)*(nt)          # vector scaling nb
                + (2*ns)*(nt-1)      # dot product
-               + ((ns-1)*ns)*(nt-1) # matrix product, ns-1*ns HALF
+               + ((ns-1)*ns)*(nt-1)  # matrix product, ns-1*ns HALF
                + (2*nb*ns)*(nt-1)   # matrix product, nb*ns
                + (2*nb**2)*(nt)     # outer product
                )
@@ -57,8 +61,8 @@ def T_flops_POBTASI(nt: int, ns: int, nb: int):
     return int(0
                + (ns**3)*(nt)           # triangular solve ns3
                + (nb**3)*(1)            # triangular solve ns2
-               + (2*ns**3)*(4*(nt-1)+1) # DGEMM ns3
-               + (2*ns**2*nb)*(4*(nt-1)+2) # DGEMM ns2nb
+               + (2*ns**3)*(4*(nt-1)+1)  # DGEMM ns3
+               + (2*ns**2*nb)*(4*(nt-1)+2)  # DGEMM ns2nb
                + (2*ns*nb**2)*(nt)      # DGEMM nsnb2
                + (2*nb**3)*(1)          # DGEMM nb3
                )
@@ -75,12 +79,21 @@ def T_flops_POBBAF(nt, ns, nb, n):
                + (2*ns*nb**2)*(nt)      # DGEMM nsnb2
                )
 
+
 def T_flops_POBBASI(nt: int, ns: int, nb: int, n):
     return int(0
                + (ns**3)*(nt)           # triangular solve ns3
                + (nb**3)*(1)            # triangular solve nb3
-               + (2*ns**3)*((nt-1)*(n+1)**2+1) # DGEMM ns3
-               + (2*ns**2*nb)*(2*(nt-1)*(n+1)+2) # DGEMM ns2nb
+               + (2*ns**3)*((nt-1)*(n+1)**2+1)  # DGEMM ns3
+               + (2*ns**2*nb)*(2*(nt-1)*(n+1)+2)  # DGEMM ns2nb
                + (2*ns*nb**2)*(nt)      # DGEMM nsnb2
                + (2*nb**3)*(1)          # DGEMM nb3
                )
+
+
+def T_FLOPS_unpack(alg: str, nt: int, ns: int, nb: int, n: int):
+    flops: int = 0
+    for op in ALG_OPERATION_COUNT[alg]:
+        flops += OPERATIONS_FLOPS[op]['flops'](ns, nb) * \
+            ALG_OPERATION_COUNT[alg][op](nt, n)
+    return int(flops)
